28bf78d438bd0cfa1fb7b6d3b7740aba36499ff7
nrxtka <nindiinurartika@gmail.com>
2025-12-15 22:20:58 +0700

    prosedur & fungsi

---
+++ php/perkenalan.php
@@ -0,0 +1,12 @@
+<?php
+
+// membuat fungsi
+function perkenalan(){
+ echo "Assalamulaikmu, ";
+ echo "Perkenalkan, nama saya Ardianta<br/>";
+ echo "Senang berkenalan dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
